61027040e693d132ad5b49b93a6588b37af0fcd1
Michael Turvey <michaelturvey97@gmail.com>
2023-03-14 17:33:46 -0400

    centered mobile divs

--- src/App.js
+++ src/App.js
@@ -32,8 +32,10 @@ function App() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+        <div class="header">
         <h1>🧙 Contrast Wizard</h1>
         <p>This tool tests 2 or 3 colors for contrast, and recommends colors with better contrast.</p>
+        </div>
         <div className="toolset">
           <div className="section choose-example">
             <h2>🧪 Test contrast for</h2>
