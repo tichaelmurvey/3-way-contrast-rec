f10984f3082de06c22c9b200b33edb69b4a49585
Michael Turvey <michaelturvey97@gmail.com>
2023-05-01 15:42:40 -0400

    added SEO

--- src/App.js
+++ src/App.js
@@ -32,9 +32,16 @@ function App() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
-        <div class="header">
+        <div className="header">
+        <div className="header-left">
         <h1>🧙 Contrast Wizard</h1>
         <p>This tool tests 2 or 3 colors for contrast, and recommends colors with better contrast.</p>
+        </div>
+        <div class="header-right">
+        <p>Created by <a target="_blank" href="https://michaelturvey.site">Michael Turvey</a></p>
+        {/* Buy me a coffee button */}
+        <a target="_blank" className="buymeacoffee" href= "https://www.buymeacoffee.com/frommountains">Buy me a coffee</a>
+        </div>
         </div>
         <div className="toolset">
           <div className="section choose-example">
